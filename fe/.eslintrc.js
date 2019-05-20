8faf7e172dc21fc48f308e88425f55eb061285ea
Dmitriy <dmitriy.chistokhodov@emlid.com>
2019-05-20 16:32:13 +0300

    fe: add new ESLint config rules
    Add rule for single argument's parens at arrow functions ("arrow-parens"), single quotes ("quotes"), imports sort ("sort-imports"), maximum line length ("max-len"), maximum attributes/properties of tag per line ("vue/max-attrbutes-per-line")

--- fe/.eslintrc.js
+++ fe/.eslintrc.js
@@ -1,12 +1,17 @@
 module.exports = {
   root: true,
-  env: {
-    node: true
-  },
-  extends: ['plugin:vue/essential', '@vue/typescript', 'airbnb-base'],
+  env: { browser: true, node: true },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/typescript',
+    'airbnb-base'
+  ],
   rules: {
     // Enforce line breaks between array elements
     'array-element-newline': ['error', { 'multiline': true, 'minItems': 3 }],
+
+    // Omit parentheses when arrow function have exactly one parameter
+    'arrow-parens': 'off',
 
     // Do not enforce class method to utilize `this`
     'class-methods-use-this': 'off',
@@ -26,12 +31,46 @@ module.exports = {
     // Ignore `import` statements with Webpack `@` alias in path
     'import/no-unresolved': ['error', { ignore: ['^@'] }],
 
+    // Set maximum line length to 120 characters. Lines containing URLs, strings, template literals or regular
+    // expressions will be ignored.
+    'max-len': ['error', {
+      'code': 120,
+      'ignoreUrls': true,
+      'ignoreStrings': true,
+      'ignoreTemplateLiterals': true,
+      'ignoreRegExpLiterals': true
+    }],
+
     // Do not permit `console` references or `debugger` statements in production bundle
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 
+    // Requires the use of single quotes wherever possible. Double quotes are allowed as long as the string contains
+    // a single quote that would have to be escaped otherwise.
+    'quotes': [
+      'error',
+      'single',
+      { 'avoidEscape': true }
+    ],
+
+    // Sorts all imports alphabetically, case-sensitively, without declaration sort
+    'sort-imports': 'error',
+
+    // Limit the maximum number of attributes/properties per line
+    'vue/max-attributes-per-line': ['error', {
+      'singleline': 1,
+      'multiline': {
+        'max': 1,
+        'allowFirstLine': false
+      }
+    }],
+
     // Base indent of 2 spaces in `.vue` files
-    'vue/script-indent': ['error', 2, { 'baseIndent': 1 }]
+    'vue/script-indent': [
+      'error',
+      2,
+      { 'baseIndent': 1 },
+    ]
   },
   'overrides': [
     {
